b2b64e94aa6ddde469529047817655a9a4fb493e
Alexander_Violeta_Tovar <alexander.violeta.tovar@gmail.com>
2016-11-06 00:36:47 -0500

    Initial commit

---
+++ app/src/main/java/mx/violeta_tovar/primeratarea/MainActivity.java
@@ -0,0 +1,13 @@
+package mx.violeta_tovar.primeratarea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
